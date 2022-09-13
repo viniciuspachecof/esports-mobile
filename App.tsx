684c8bbd8140e9db9923df006af7f267a6f024a9
Vinicius Pacheco <vinicius@pachecoof@hotmail.com>
2022-09-13 19:16:51 -0300

    primeiro commit (preparando o setup)

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+interface IButton {
+  text: string;
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Dale</Text>
+      <Button text='hehehehe' />
       <StatusBar style="auto" />
     </View>
   );
+}
+
+function Button(props: IButton) {
+  return (
+    <Text>{props.text}</Text>
+  )
 }
 
 const styles = StyleSheet.create({
