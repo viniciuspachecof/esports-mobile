a5db225c40a7a4e64baa072a733f0ae7eea04a6a
Vinicius Pacheco <vinicius@pachecoof@hotmail.com>
2022-09-13 22:59:02 -0300

    Iniciando a criação da interface do app

--- App.tsx
+++ App.tsx
@@ -1,32 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-interface IButton {
-  text: string;
-}
+import { StatusBar } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
+import { Background } from './src/components/Background';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Dale</Text>
-      <Button text='hehehehe' />
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+      { fontsLoaded ? <Home /> : <Loading />}
+    </Background>
   );
 }
-
-function Button(props: IButton) {
-  return (
-    <Text>{props.text}</Text>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
